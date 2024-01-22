5893a4456f48de71243200b4c6b287f2a816597b
domenicolafroscia <domenicolafroscia@gmail.com>
2024-01-22 18:41:22 +0100

    add: validation for editing a form

---
+++ app/Http/Requests/UpdateComicRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|min:5|max:250',
+            'description' => 'nullable',
+            'thumb' => 'required',
+            'price' => 'required',
+            'series' => 'required',
+            'sale_date' => 'required',
+            'type' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'The title is obligatory',
+            'title.min' => 'The title must be at least :min characters long',
+            'title.max' => 'The title is too long, max :max characters',
+            'thumb.required' => "The image is obligatory",
+            'price.required' => "The price is obligatory",
+            'series.required' => "The series is obligatory",
+            'sale_date.required' => "The sale date is obligatory",
+            'type.required' => "The type is obligatory, choose an option from those available"
+        ];
+    }
+}
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -6,45 +6,60 @@
 
         <div class="row justify-content-center mt-5">
             <div class="col-6">
+
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
                 <form action="{{ route('comics.update', ['comic' => $comic->id]) }}" method="POST">
                     @csrf
                     @method('PUT')
 
                     <div class="mb-3">
                         <label for="title" class="form-label">Title</label>
-                        <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
+                        <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $comic->title) }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="description" class="form-label">Description</label>
-                        <textarea class="form-control" id="description" rows="3" name="description">{{ $comic->description }}</textarea>
+                        <textarea class="form-control" id="description" rows="3" name="description">{{ old('description', $comic->description) }}</textarea>
                     </div>
 
                     <div class="mb-3">
                         <label for="thumb" class="form-label">Image</label>
-                        <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comic->thumb }}">
+                        <input type="text" class="form-control" id="thumb" name="thumb"
+                            value="{{ old('thumb', $comic->thumb) }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="price" class="form-label">Price</label>
-                        <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price }}">
+                        <input type="text" class="form-control" id="price" name="price"
+                            value="{{ old('price', $comic->price) }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="series" class="form-label">Series</label>
-                        <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series }}">
+                        <input type="text" class="form-control" id="series" name="series"
+                            value="{{ old('series', $comic->series) }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="sale_date" class="form-label">Sale date</label>
-                        <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}">
+                        <input type="date" class="form-control" id="sale_date" name="sale_date"
+                            value="{{ old('sale_date', $comic->sale_date) }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="type" class="form-label">Type</label>
                         <select id="type" class="form-select" name="type">
-                            <option @selected($comic->type === 'Comic book') value="comic book">Comic book</option>
-                            <option @selected($comic->type === 'graphic novel') value="graphic novel">Graphic novel</option>
+                            <option @selected(old('type', $comic->type) === 'Comic book') value="comic book">Comic book</option>
+                            <option @selected(old('type', $comic->type) === 'graphic novel') value="graphic novel">Graphic novel</option>
                         </select>
                     </div>
 
